78075d04f3adad4192704c374391962f30bd788d
Antonio Arias <antonio.arias@gmail.com>
2019-02-23 15:37:49 +0100

    Added function get_file_number_lines.

--- csv-transformer.py
+++ csv-transformer.py
@@ -1,6 +1,12 @@
 import csv
 
 
+def get_file_number_lines(filename):
+    with open(filename) as csv_file:
+        file_data = csv.reader(csv_file)
+        return sum(1 for line in file_data)
+
+
 def get_file_data(filename):
     with open(filename) as csvfile:
         file_data = csv.reader(csvfile)
